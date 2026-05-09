ebf83a68bcdf804b39ef9da039e2f97e46655c96
Ilya Dosyak <ilyadosyak@gmail.com>
2026-05-09 12:29:52 +0300

    Добавили props для ToDoItem и ToDoList

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -4,6 +4,12 @@ import AddTaskForm from "./AddTaskForm";
 import SearchTaskForm from "./SearchTaskForm";
 
 const ToDo = () => {
+  const tasks = [
+    { id: 1, title: "Buy groceries", isDone: false },
+    { id: 2, title: "Walk the dog", isDone: true },
+    { id: 3, title: "Finish project", isDone: false },
+  ];
+
   return (
     <div className="todo">
       <h1 className="todo__title">To Do List</h1>
@@ -14,7 +20,7 @@ const ToDo = () => {
 
       <ToDoInfo />
 
-      <ToDoList />
+      <ToDoList tasks={tasks} />
     </div>
   );
 };
