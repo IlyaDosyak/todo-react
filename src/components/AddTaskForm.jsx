b0a5fdc38c90580dc6b495318e11d848801ae2d6
Ilya Dosyak <ilyadosyak@gmail.com>
2026-05-09 12:18:15 +0300

    Добавили props для Field

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 const AddTaskForm = () => {
   return (
     <form className="todo__form">
-      <Field />
+      <Field className="todo__field" id="new-task" label="New task" />
       <Button />
     </form>
   );
--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-
-const Field = () => {
+const Field = (props) => {
+  const { className, id, label, type = "text" } = props;
   return (
-    <div className="todo__field field">
-      <label className="field__label" htmlFor="new-task">
-        New task
+    <div className={`field ${className}`}>
+      <label className="field__label" htmlFor={id}>
+        {label}
       </label>
       <input
         className="field__input"
-        id="new-task"
+        id={id}
+        type={type}
         placeholder=" "
         autoComplete="off"
       />
